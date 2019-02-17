d82418d1a358fc68f0a392c266e0b6cb8e70844c
Jorge Alejandro Preciado Lopez <alexpreciado1@gmail.com>
2019-02-17 16:28:56 -0500

    Add functionaly to load a campaign given a campaignURL, and to make a copy of a Campaign object

--- cwdprophet/campaign.py
+++ cwdprophet/campaign.py
@@ -2,7 +2,38 @@
 # Created by Jorge Alejandro Preciado Lopez on 2019-02-17.
 
 class Campaign:
+	'''
+	Attributes:
+	'''
 
-	def __init__(self):
+	def __init__(self,campaign_url=None):
+		self.campaign_url = campaign_url
 	
+	def copy(self):
+
+		"""
+		Return a copy of the Campaign object.
+		Args:
+			
+		Returns:
+			(Campaign): copy of the Image.
+		"""
+		# Make new  image with primary polarization
+		newcampaign = Campaign(self.campaign_url)
+		return newcampaign
+
 		
+def load_campaign(campaign_url,display=False):
+
+	"""
+	Read in an image from a text file.
+	Args:
+		url (str): URL of campaign
+	Returns:
+		(Campaign): loaded Campaign object
+	"""
+
+
+	print('Loaded campaign from:',campaign_url)
+	
+	return Campaign(campaign_url=campaign_url)
